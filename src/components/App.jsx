ee30ae09d079e867906c88dc0d9a8f144dd0a2ac
Shegda Igor <igorbeef@gmail.com>
2023-01-06 20:11:48 +0200

    add searchbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import { Button } from './Button/Button';
+import Searchbar from './Searchbar/Searchbar';
 
 export class App extends Component {
+  processSubmit = query => {
+    this.setState({ query });
+  };
   render() {
-    return <Button onLoadMore={this.handleLoadMore} />;
+    return (
+      <>
+        <Searchbar onSubmit={this.processSubmit} />
+
+        <Button onLoadMore={this.handleLoadMore} />
+      </>
+    );
   }
 }
