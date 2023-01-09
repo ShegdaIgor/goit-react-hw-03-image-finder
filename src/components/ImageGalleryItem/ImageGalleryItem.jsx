fbc79b685a335141383d77ffecf7aca2c5e99f79
Shegda Igor <igorbeef@gmail.com>
2023-01-09 18:44:26 +0200

    add styles

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,14 +1,39 @@
 import PropTypes from 'prop-types';
 import css from './ImageGalleryItem.module.css';
+import { Modal } from '../Modal/Modal';
+import { Component } from 'react';
 
-export const ImageGalleryItem = ({ webformatURL }) => {
-  return (
-    <li className={css.imageGalleryItem}>
-      <img className={css.imageGalleryItemImage} src={webformatURL} alt="" />
-    </li>
-  );
-};
+export class ImageGalleryItem extends Component {
+  state = {
+    isModalOpen: false,
+  };
+
+  onToggleModal = () => {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
+    });
+  };
+
+  render() {
+    return (
+      <li className={css.imageGalleryItem} onClick={this.onToggleModal}>
+        <img
+          className={css.imageGalleryItemImage}
+          src={this.props.webformatURL}
+          alt=""
+        />
+        {this.state.isModalOpen && (
+          <Modal
+            largeImageURL={this.props.largeImageURL}
+            onClose={this.onToggleModal}
+          />
+        )}
+      </li>
+    );
+  }
+}
 
 ImageGalleryItem.propTypes = {
   webformatURL: PropTypes.string.isRequired,
+  largeImageURL: PropTypes.string.isRequired,
 };
